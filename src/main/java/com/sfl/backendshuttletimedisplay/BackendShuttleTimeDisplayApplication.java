6205357c01cb8a4155b782f06b4a2cdd1eada6c2
Quentin REY <quentin.rey@etu.emse.fr>
2023-01-27 21:14:00 +0100

    Initial commit

---
+++ src/main/java/com/sfl/backendshuttletimedisplay/BackendShuttleTimeDisplayApplication.java
@@ -0,0 +1,13 @@
+package com.sfl.backendshuttletimedisplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendShuttleTimeDisplayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendShuttleTimeDisplayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sfl/backendshuttletimedisplay/BackendShuttleTimeDisplayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sfl.backendshuttletimedisplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendShuttleTimeDisplayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
